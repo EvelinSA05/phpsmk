a81f1c4da53751c5b1ae2233e225394124832fe1
Evelin Salsabila A <108512588+EvelinSA05@users.noreply.github.com>
2022-10-19 20:26:52 +0700

    phpsmk bagian 20

---
+++ phpsmk-bag20/restoran/function.php
@@ -0,0 +1,11 @@
+<?php 
+
+    $host = "localhost:8111";
+    $user = "root";
+    $password = "";
+    $database = "dbrestoran";
+
+    $koneksi = mysqli_connect($host,$user,$password,$database);
+
+
+?>
